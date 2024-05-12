703e4ef0d5e55b1458e5c0fb35d6a0af9c211dc7
bishalpatra <86684382+newworld23@users.noreply.github.com>
2024-05-12 12:17:03 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const AccountId = 12223
+let AccountEmail = "hdsdjhfkjsdjfhjsd"
+var AccountPassword = "123450"
+accountCity= "jaipur"
+
+
+
+//AccountId = 2//
+AccountEmail ="hmjnjnj"
+AccountPassword = "252626260"
+accountCity ="Bengaluru"
+ 
+
+console.table([AccountEmail, AccountId ,AccountPassword])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+//alert( 3+3 )// we are using nodejs ,not browser
+// code must be readable
+
+//datatypes: premitive
+
+let name = "hitesh" // string
+let age = 18 // number => 2 to power 53, bigint i.e integer
+let isLoggedIn = false // boolean ==> true/false
+
+// null : its a standalone  value
+ // undefined ==>
+// symbol==> unique
+
+ //object
+
+ console.table([typeof undefined, typeof null])
