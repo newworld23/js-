0f78e4de8a6ee4d3022890b9793e953f4eaa695e
bishalpatra <86684382+newworld23@users.noreply.github.com>
2024-05-12 16:19:00 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+/*
+In this code snippit we are mainly focused on  
+converting the datatypes from one to another and
+ then checking them using"typeof" keyword that returns 
+ the type of the variable.
+*/
+
+
+
+/*
+let score = "abc";
+
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);// output will give us NaN
+*/
+//notes
+/*
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0
+*/
+let isLoggedIn = 1
+ let booleanIsLoggedIn = Boolean(isLoggedIn);
+ console.log(booleanIsLoggedIn)
+ //1 => true; 0 => false;
+ //"" => false
+ //"sfd" => true 
